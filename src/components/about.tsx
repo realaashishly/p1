08af61684a3d33a862c5f7dd730c80875b17cd73
Aashish Singh <11aashish.singh@gmail.com>
2025-02-09 19:12:47 +0530

    fix responsiveness

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,45 +3,49 @@ import Card from './card';
 
 export default function About() {
     return (
-        <div className='flex justify-between my-24 gap-12'>
-            <div className=' w-[50%]'>
-                <h3 className='text-zinc-600 text-2xl '>
-                    My passion lies in the intersection of art and technology,
-                    creating visually captivating interfaces and elevating
-                    overall user digital experiences.
-                </h3>
-                <h3 className=' text-xl mt-10'>
-                    With a Bachelor of Application and currently pursuing my
-                    Master of Application at Mahatma Gandhi Kashi Vidyapith in
-                    Varanasi, I have built a solid foundation in digital
-                    technology and creative design. As a dedicated software
-                    developer and visionary AI artist, I leverage cutting-edge
-                    tools like Midjourney and Playground to craft stunning
-                    AI-generated imagery. This unique blend of technical
-                    expertise and artistic passion allows me to seamlessly merge
-                    innovation with aesthetics, delivering digital experiences
-                    that are both powerful and visually captivating. I am
-                    committed to pushing the boundaries of creativity and
-                    technology, transforming ideas into immersive, artful
-                    solutions.
-                </h3>
-            </div>
-            <div className='w-[50%] flex flex-col space-y-6'>
-                <Card
-                    head='Development'
-                    subhead='Designing, building, and deploying state-of-the-art applications using modern frameworks, microservices, and DevOps best practices.'
-                    percentage={93}
-                />
-                <Card
-                    head='Designing'
-                    subhead='Crafting engaging digital experiences and visually captivating interfaces that merge aesthetics with functionality.'
-                    percentage={80}
-                />
-                <Card
-                    head='AI Creation'
-                    subhead='Leveraging advanced generative AI and machine learning techniques to create immersive digital art and interactive solutions.'
-                    percentage={85}
-                />
+        <div className='my-24 px-4'>
+            <div className='flex flex-col md:flex-row justify-between gap-12 max-w-7xl mx-auto'>
+                {/* Text Section */}
+                <div className='w-full md:w-1/2'>
+                    <h3 className='text-zinc-600 text-lg md:text-2xl leading-relaxed'>
+                        My passion lies in the intersection of art and
+                        technology, creating visually captivating interfaces and
+                        elevating overall digital experiences.
+                    </h3>
+                    <h3 className='mt-10 text-base md:text-xl leading-relaxed'>
+                        With a Bachelor of Application and currently pursuing my
+                        Master of Application at Mahatma Gandhi Kashi Vidyapith
+                        in Varanasi, I have built a solid foundation in digital
+                        technology and creative design. As a dedicated software
+                        developer and visionary AI artist, I leverage
+                        cutting-edge tools like Midjourney and Playground to
+                        craft stunning AI-generated imagery. This unique blend
+                        of technical expertise and artistic passion allows me to
+                        seamlessly merge innovation with aesthetics, delivering
+                        digital experiences that are both powerful and visually
+                        captivating. I am committed to pushing the boundaries of
+                        creativity and technology, transforming ideas into
+                        immersive, artful solutions.
+                    </h3>
+                </div>
+                {/* Cards Section */}
+                <div className='w-full md:w-1/2 flex flex-col space-y-6'>
+                    <Card
+                        head='Development'
+                        subhead='Designing, building, and deploying state-of-the-art applications using modern frameworks, microservices, and DevOps best practices.'
+                        percentage={93}
+                    />
+                    <Card
+                        head='Designing'
+                        subhead='Crafting engaging digital experiences and visually captivating interfaces that merge aesthetics with functionality.'
+                        percentage={80}
+                    />
+                    <Card
+                        head='AI Creation'
+                        subhead='Leveraging advanced generative AI and machine learning techniques to create immersive digital art and interactive solutions.'
+                        percentage={85}
+                    />
+                </div>
             </div>
         </div>
     );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,87 +3,74 @@
 import Image from 'next/image';
 import Star from '../../public/star.png';
 
-export default function Hero() {   
-    return (
-        <div className='flex flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-24 space-y-6'
-        >
-            {/* Top Section */}
-            <div className='flex flex-col items-center my-12'>
-                <div className='flex flex-wrap items-center gap-2 text-zinc-600 text-xs sm:text-sm'>
-                    {[
-                        'CRAFTING',
-                        'DIGITAL',
-                        'GOODS',
-                        'SINCE',
-                        '—',
-                        'Y:2017',
-                    ].map((text, index) => (
-                        <span key={index}>{text}</span>
-                    ))}
-                </div>
-                <div className='relative text-center mt-4'
-                
-                >
-                    <h1 className='whitespace-nowrap leading-none font-bold uppercase text-yellow text-5xl sm:text-6xl md:text-8xl lg:text-[205px]'>
-                        {['A', 'A', 'S', 'H', 'I', 'S', 'H'].map(
-                            (char, index) => (
-                                <span
-                                    key={`first-${index}`}
-                                    className='inline-block'
-                                >
-                                    {char}
-                                </span>
-                            )
-                        )}
-                        <br />
-                        {['S', 'I', 'N', 'G', 'H'].map((char, index) => (
-                            <span
-                                key={`second-${index}`}
-                                className='inline-block'
-                            >
-                                {char}
-                            </span>
-                        ))}
-                    </h1>
-                    <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                        <Image
-                            src='/profile.png'
-                            alt='Profile photo of Aashish Singh'
-                            width={200}
-                            height={200}
-                            className='rounded-full'
-                        />
-                    </div>
-                </div>
-            </div>
-
-            {/* Bottom Section */}
-            <div className='flex flex-col items-center justify-between space-y-16'>
-                <div>
-                    <Image
-                        src={Star}
-                        alt='Star'
-                        width={40}
-                        height={40}
-                        className='w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16'
-                    />
-                </div>
-
-                <div>
-                    <p className='text-base sm:text-lg md:text-2xl text-white text-center mx-auto w-11/12 md:w-8/12'>
-                        I’m Aashish Singh — a passionate software developer and
-                        AI artist, dedicated to crafting innovative digital
-                        experiences and intelligent solutions.
-                    </p>
-                </div>
-
-                <div className='flex flex-col items-center text-zinc-600'>
-                    <div className='flex justify-center w-1 bg-zinc-600 border h-8 overflow-hidden'>
-                        <div className='w-1 h-4 bg-yellow animate-scrolling13'></div>
-                    </div>
-                    <span className='mt-2 text-xs sm:text-sm'>Scroll Down</span>
-                </div>
-            </div>
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-24 space-y-6">
+      {/* Top Section */}
+      <div className="flex flex-col items-center my-12 w-full max-w-4xl mx-auto">
+        <div className="flex flex-wrap items-center gap-2 text-zinc-600 text-xs sm:text-sm">
+          {[
+            'CRAFTING',
+            'DIGITAL',
+            'GOODS',
+            'SINCE',
+            '—',
+            'Y:2017',
+          ].map((text, index) => (
+            <span key={index}>{text}</span>
+          ))}
         </div>
-    );
+        <div className="relative text-center mt-4">
+          <h1 className="whitespace-nowrap leading-none font-bold uppercase text-yellow text-5xl sm:text-6xl md:text-8xl lg:text-[205px]">
+            {['A', 'A', 'S', 'H', 'I', 'S', 'H'].map((char, index) => (
+              <span key={`first-${index}`} className="inline-block">
+                {char}
+              </span>
+            ))}
+            <br />
+            {['S', 'I', 'N', 'G', 'H'].map((char, index) => (
+              <span key={`second-${index}`} className="inline-block">
+                {char}
+              </span>
+            ))}
+          </h1>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <Image
+              src="/profile.png"
+              alt="Profile photo of Aashish Singh"
+              width={200}
+              height={200}
+              className="rounded-full border-2 border-yellow-500"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className="flex flex-col items-center justify-between space-y-16 w-full max-w-4xl mx-auto">
+        <div>
+          <Image
+            src={Star}
+            alt="Star"
+            width={40}
+            height={40}
+            className="w-10 h-10 md:w-12 md:h-12 lg:w-16 lg:h-16"
+          />
+        </div>
+        <div>
+          <p className="text-base sm:text-lg md:text-2xl text-white text-center mx-auto w-11/12 md:w-8/12">
+            I’m Aashish Singh — a passionate software developer and AI artist,
+            dedicated to crafting innovative digital experiences and intelligent
+            solutions.
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-zinc-600">
+          <div className="flex justify-center w-1 bg-zinc-600 border h-8 overflow-hidden">
+            <div className="w-1 h-4 bg-yellow animate-scrolling13"></div>
+          </div>
+          <span className="mt-2 text-xs sm:text-sm">Scroll Down</span>
+        </div>
+      </div>
+    </div>
+  );
 }
