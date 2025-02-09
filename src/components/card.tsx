1c2388f59bd694c815a09c7c92692f7aca5af3dc
Aashish Singh <11aashish.singh@gmail.com>
2025-02-09 19:25:16 +0530

    added responsiveness

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,13 +6,18 @@ interface CardProps {
 
 export default function Card({ head, subhead, percentage }: CardProps) {
     return (
-        <div className="bg-yellow p-4 rounded-lg flex justify-between items-center">
-            <div className="flex flex-col space-y-4 w-64">
-                <h4 className="text-black text-2xl font-medium">{head}</h4>
-                <p className="text-zinc-900 text-base">{subhead}</p>
+        <div className='bg-yellow-100 p-6 rounded-lg flex flex-col md:flex-row justify-between items-center gap-4'>
+            {/* Text Section */}
+            <div className='flex flex-col space-y-2 flex-1'>
+                <h4 className='text-black text-xl md:text-2xl font-medium'>
+                    {head}
+                </h4>
+                <p className='text-zinc-900 text-sm md:text-base'>{subhead}</p>
             </div>
-            <div className="w-[1px] h-12 bg-zinc-600 rounded-full" />
-            <div className="text-6xl text-zinc-600/60">
+            {/* Vertical Divider - visible on md screens and up */}
+            <div className='hidden md:block w-px h-12 bg-zinc-600' />
+            {/* Percentage Section */}
+            <div className='text-4xl md:text-6xl text-zinc-600/60'>
                 <p>{percentage}%</p>
             </div>
         </div>
